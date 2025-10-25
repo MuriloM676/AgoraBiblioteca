ff345077f525b5853a052bedc6cb1a19d8160102
Murilo Martins <murilom676@gmail.com>
2025-10-25 10:04:28 -0300

    chore(gitignore): ignore storage/framework cache & views; untrack compiled Blade caches

--- storage/framework/views/d1bfe3fb93efaf828a0058cbc5f7e226.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['flex gap-x-1'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH C:\Users\muril\Downloads\AgoraBiblioteca\vendor\filament\forms\resources\views/components/rich-editor/toolbar/group.blade.php ENDPATH**/ ?>
